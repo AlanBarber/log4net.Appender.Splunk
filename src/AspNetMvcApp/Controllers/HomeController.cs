c5b5b68b4320c29bc037a4bf4f2411d28850bb18
Alan P. Barber <barber.alan@outlook.com>
2018-11-21 13:42:18 -0500

    adding asp.net mvc demo project

---
+++ src/AspNetMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(HomeController));
+
+        public ActionResult Index()
+        {
+            log.Info("Action Index has been fired.");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
