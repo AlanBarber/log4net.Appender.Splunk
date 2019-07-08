9cd3b1c2e0d6a9fd3208c959e6ee0e7f1ffa17d6
Dmitry Nechaev <dmitry.nechaev@jax.com.au>
2019-07-08 11:25:38 +1000

    updated code comment

--- src/log4net.Appender.Splunk/SplunkHttpEventCollector.cs
+++ src/log4net.Appender.Splunk/SplunkHttpEventCollector.cs
@@ -86,7 +86,7 @@ namespace log4net.Appender.Splunk
 
             // Send the event to splunk
             _hecSender.Send(DateTime.Now, null, loggingEvent.Level.Name, null, loggingEvent.RenderedMessage, loggingEvent.ExceptionObject, properties, metaData);
-            // the awaiter will lock the code execution and break batch processing
+            // the sync waiting will lock the code execution and break batch processing
             //_hecSender.FlushSync();
         }
 
